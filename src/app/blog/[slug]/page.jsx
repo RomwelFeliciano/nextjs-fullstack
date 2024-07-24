300d9095049b0666bca13f0dccd6e4de667b92ae
RomwelFeliciano <romfeliciano18@gmail.com>
2024-07-24 14:36:35 +0800

    Data Fetching: API

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import Image from 'next/image';
+import { Suspense } from 'react';
+import styles from './singlePost.module.css';
+import PostUser from '@/components/postUser/PostUser';
 
-const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+const getData = async (slug) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${slug}`,
+    {
+      next: { revalidate: 3600 }, //refresh data every hour
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error('Something went wrong!');
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+
+  const post = await getData(slug);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src='https://images.pexels.com/photos/1122411/pexels-photo-1122411.jpeg?auto=compress&cs=tinysrgb&w=600'
+          alt=''
+          fill
+          className={styles.img}
+        />
+      </div>
+
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <PostUser userId={post.userId} />
+          </Suspense>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.body}</div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
---
+++ src/components/postUser/PostUser.jsx
@@ -0,0 +1,39 @@
+import styles from './postUser.module.css';
+import Image from 'next/image';
+
+const getData = async (userId) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`,
+    {
+      cache: 'no-store',
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error('Something went wrong!');
+  }
+
+  return res.json();
+};
+
+const PostUser = async ({ userId }) => {
+  const user = await getData(userId);
+
+  return (
+    <div className={styles.container}>
+      <Image
+        className={styles.avatar}
+        src='https://images.pexels.com/photos/1122411/pexels-photo-1122411.jpeg?auto=compress&cs=tinysrgb&w=600'
+        alt=''
+        width={50}
+        height={50}
+      />
+      <div className={styles.texts}>
+        <span className={styles.title}>Author</span>
+        <span className={styles.username}>{user.username}</span>
+      </div>
+    </div>
+  );
+};
+
+export default PostUser;
