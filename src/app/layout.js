f1537d4a575ca91faee91fc5ac65d3153128875f
RomwelFeliciano <romfeliciano18@gmail.com>
2024-08-05 14:59:03 +0800

    Google Analytics: Add

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <GoogleAnalytics />
       <body className={inter.className}>
         <div className='container'>
           <Navbar />
